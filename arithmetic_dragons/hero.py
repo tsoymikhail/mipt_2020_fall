3d8e479b82f4a8086de24db37323d789f26d2d95
tsoymikhail <56151708+tsoymikhail@users.noreply.github.com>
2020-12-20 22:33:00 +0300

    Add files via upload

---
+++ arithmetic_dragons/hero.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# license: GPLv3
+from gameunit import *
+
+#FIXME:
+"""В этом файле должен быть описан класс героя, унаследованный от Attacker
+Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
+Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
+
+"""
+class Hero(Attacker):
+    _experience = ''
+    _name = ''
+    
+    def __init__(self, name):
+        self._name = name
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
+        
+    def attack(self, target):
+        target._health -= self._attack
